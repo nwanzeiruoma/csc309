409634d4326063ddbacfd1496785db14924b6c31
Ogbonn Michael <127021160+charsacdev@users.noreply.github.com>
2023-07-17 09:42:06 +0100

    select code fetching all users data

--- index.php
+++ index.php
@@ -9,16 +9,58 @@
     <!-- Select all users and display in a Table -->
     <table>
         <thead>
-            <!-- Complete the table header row -->
+            <tr>
+                <td>SN</td>
+                <td>Last Name</td> 	
+                <td>First Name</td> 	
+                <td>Gender</td>
+                <td>Date of Birth</td> 	
+                <td>Email</td>
+            </tr>
         </thead>
 
         <tbody>
+           <?php
+                //my server variables
+                $server = 'localhost'; // 127.0.0.1
+                $username = 'root';
+                $password = 'rootroot';
+                $db = 'csc309';
+
+            //my server connection
+            $conn = new mysqli($server, $username, $password, $db);
+            
+            //check server connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            
+            //my select query
+            $sql = "SELECT * FROM users";
+            
+            //query result
+            $result = $conn->query($sql);
+            
+            // Check if the query was successful
+            if ($result === false) {
+                die("Error: " . $conn->error);
+            }
+            //fetch the data from the result set
+            while ($row = $result->fetch_assoc()) {?>
+                 <tr>
+                    <td><?php $row["id"];?></td>
+                    <td><?php $row["lastname"]</td> 	
+                    <td><?php $row["firstname"]</td> 	
+                    <td><?php $row["gender"];?></td>
+                    <td><?php $row["date_of_birth"];?></td> 	
+                    <td><?php $row["email"];?></td>
+               </tr>
             <?php
-                // Complete code to:
-                // Connect to the DB
-                // Select all records from users table
-                // Display all the selected records
+                }
+                // Close the database connection
+                $conn->close();
             ?>
+
         </tbody>
     </table>
 </body>
